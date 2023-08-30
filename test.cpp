b5a1b617833291141be00996728f0fa64f884633
Abhisek753 <properachievement@gmail.com>
2023-08-30 21:16:54 +0530

    data type

--- test.cpp
+++ test.cpp
@@ -7,21 +7,27 @@ using namespace std;
 
 
 int main(){
-    int a=123;
-    cout <<a <<endl;
+    // int a=123;
+    // cout <<a <<endl;
 
-    char b = 'v';
-    cout << b <<endl;
+    // char b = 'v';
+    // cout << b <<endl;
 
-    bool bl=true;
-    cout <<bl<<endl;
+    // bool bl=true;
+    // cout <<bl<<endl;
 
-     float f=1.2;
-     cout <<f <<endl;
+    //  float f=1.2;
+    //  cout <<f <<endl;
 
-     double d=1.23;
-     cout<<d<<endl;
+    //  double d=1.23;
+    //  cout<<d<<endl;
 
-     int size=sizeof(a);
-     cout <<"Size of a is :"<<size<<endl;
+    //  int size=sizeof(a);
+    //  cout <<"Size of a is :"<<size<<endl;
+
+    int a='a';
+    cout <<a<<endl;
+
+    char ch=98;
+    cout <<ch<<endl;
 }
