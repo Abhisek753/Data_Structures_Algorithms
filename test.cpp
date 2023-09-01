f3fa1083355b8018343890cc1ce2c8836118f853
Abhisek753 <properachievement@gmail.com>
2023-09-01 23:07:47 +0530

    data add

--- test.cpp
+++ test.cpp
@@ -6,58 +6,52 @@ using namespace std;
 
 
 
-// int main(){
-//     // int a=123;
-//     // cout <<a <<endl;
+int main(){
+    // int a=123;
+    // cout <<a <<endl;
 
-//     // char b = 'v';
-//     // cout << b <<endl;
+    // char b = 'v';
+    // cout << b <<endl;
 
-//     // bool bl=true;
-//     // cout <<bl<<endl;
+    // bool bl=true;
+    // cout <<bl<<endl;
 
-//     //  float f=1.2;
-//     //  cout <<f <<endl;
+    //  float f=1.2;
+    //  cout <<f <<endl;
 
-//     //  double d=1.23;
-//     //  cout<<d<<endl;
+    //  double d=1.23;
+    //  cout<<d<<endl;
 
-//     //  int size=sizeof(a);
-//     //  cout <<"Size of a is :"<<size<<endl;
+    //  int size=sizeof(a);
+    //  cout <<"Size of a is :"<<size<<endl;
 
-//     int a='a';
-//     cout <<a<<endl;
+    // int a='a';
+    // cout <<a<<endl;
 
-//     char ch=98;
-//     cout <<ch<<endl;
+    // char ch=98;
+    // cout <<ch<<endl;
 
-//     char ch1=123456;
-//     cout << ch1 <<endl;
-// }
-int findMax(const vector<int>& arr) {
-    int maxElement = arr[0]; 
-    
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] > maxElement) {
-            maxElement = arr[i]; 
+    // char ch1=123456;
+    // cout << ch1 <<endl;
+     int sideLength;
+
+    // Input the side length of the square
+    std::cout << "Enter the side length of the square: ";
+    std::cin >> sideLength;
+
+    // Check for valid input
+    if (sideLength <= 0) {
+        std::cout << "Please enter a positive integer for side length." << std::endl;
+        return 1; 
+    }
+
+    // Loop to print the square pattern
+    for (int i = 0; i < sideLength; i++) {
+        for (int j = 0; j < sideLength; j++) {
+            std::cout << "* ";
         }
+        std::cout << std::endl;
     }
-    
-    return maxElement;
-}
-int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    
-    vector<int> arr(n);
-    cout << "Enter the elements:\n";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    
-    int maxElement = findMax(arr);
-    cout << "The maximum element in the array is: " << maxElement << endl;
-    
-    return 0;
+
+    return 0; 
 }
